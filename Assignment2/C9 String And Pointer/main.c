e9a1a2b3390b85eaab02670ee7491d2a4254b0b1
E1PsyCongroo <92860628+E1PsyCongroo@users.noreply.github.com>
2023-03-07 19:40:32 +0800

    init

---
+++ Assignment2/C9 String And Pointer/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    char a[4] = {"srt"};
+    if (a == "srt"){
+        printf("yes\n");
+    } else{
+        printf("no\n");
+    }
+    return EXIT_SUCCESS;
+}
