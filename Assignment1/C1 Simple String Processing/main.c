a893b9818f3c6de6a0ec24e7ac695c256941e5a2
E1PsyCongroo <elpsycongroo242@gmail.com>
2023-08-29 11:59:41 +0800

    init

--- Assignment1/C1 Simple String Processing/main.c
+++ Assignment1/C1 Simple String Processing/main.c
@@ -5,12 +5,12 @@ int main(void)
 {
   char ch = 0;
   int sum_other = 0; // other char
-  int sum_space = 0; // space char ' '、'  '
+  int sum_space = 0; // space char ' '、'\t'
   int sum_enter = 0; // enter char '\n'
   printf("Please Enter the string (# to quit)\n"); // # to quit
   /*
     your codes here
   */
-  printf("space = %d,enter = %d,other = %d\n", sum_space, sum_enter, sum_other);  
+  printf("space = %d,enter = %d,other = %d\n", sum_space, sum_enter, sum_other);
   return EXIT_SUCCESS;
 }
